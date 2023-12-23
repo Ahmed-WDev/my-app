c774246b7a1e9107e2e0edbcc56c93d2f2c118db
Ahmed <ahmedbouterfa20@gmail.com>
2023-12-23 12:34:47 +0100

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,18 @@
+
+function Home() {
+  
+  return (
+    <main >
+      <h1 className="css">Project Counter</h1>
+      <hr />
+      <h2>Hello World</h2>
+      <div className="container">
+        <h3>ahmed academy</h3>
+        <h3>ahmed web developer</h3>
+      </div>
+      
+    </main>
+  );
+}
+
+export default Home;
